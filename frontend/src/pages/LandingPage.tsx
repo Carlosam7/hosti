45f0c87eebf5418d114a9799e00b976fe1edc0cf
Carlosam7 <carlosarango1107@outlook.com>
2025-11-23 01:22:50 -0500

    Tailwindcss installation fix

---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,28 @@
+function LandingPage () {
+    return (
+        <>
+            <main className="box-border">
+                <section className={`flex justify-center items-end w-screen h-screen bg-[url("assets/backgrounds/SL-120722-54440-04.jpg")] bg-cover bg-center`}>
+                    <img src="#" alt="logo" />
+                    <h1></h1>
+                    <p></p>
+                    <div>
+                        <button>
+                            Contact us
+                        </button>
+                        <button>
+                            Get start
+                        </button>
+                    </div>
+                </section>
+                <section className={`w-screen h-screen bg-orange-700`}>
+                </section>
+                <section className={`w-screen h-screen bg-orange-700`}>
+
+                </section>
+            </main>
+        </>
+    )
+}
+
+export default LandingPage

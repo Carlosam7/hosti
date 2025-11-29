802c5675286498e96c165087c386cc5218b4e371
edadul <edadul@uninorte.edu.co>
2025-11-29 15:31:13 -0500

    Update refresh route

--- backend/src/auth/auth.routes.ts
+++ backend/src/auth/auth.routes.ts
@@ -6,7 +6,7 @@ const authController = new AuthController();
 
 router.post("/login", authController.login);
 router.post("/signup", authController.signup);
-router.post("/refresh-token", authController.refreshToken);
+router.post("/refresh", authController.refreshToken);
 router.post("/logout", authController.logout);
 router.get("/me", authController.authMe);
 
